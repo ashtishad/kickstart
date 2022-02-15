ced52ca8d0ca8369517512656db6c60aee3a72ac
ashtishad <ashefhtishad@gmail.com>
2022-02-15 18:12:39 +0600

    Practice Problem 2: Centauri Prime, testcases 1 passed, testcase 2 WA

---
+++ practice/centauri_prime/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var T int
+	fmt.Scanf("%d", &T)
+	for t := 1; t <= T; t++ {
+		var k string
+		fmt.Scanf("%s\n", &k)
+
+		fmt.Printf("Case #%d: %s %s %s%s\n", t, k, "is ruled by", getRuler(k), ".")
+	}
+}
+
+func getRuler(k string) string {
+	last := k[len(k)-1]
+
+	switch last {
+	case 'a', 'e', 'i', 'o', 'u':
+		return "Alice"
+	case 'y':
+		return "nobody"
+	default:
+		return "Bob"
+	}
+}
